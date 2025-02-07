0ccc098b97319e6691ceb535f68630f7e81c2047
Mauricio Togneri <mauricio.togneri@gmail.com>
2025-02-07 21:49:23 +0100

    Improvements

--- frontend/lib/presentation/requirements/requirements_view.dart
+++ frontend/lib/presentation/requirements/requirements_view.dart
@@ -1,10 +1,12 @@
 import 'package:dafluta/dafluta.dart';
 import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 import 'package:testflow/debug/data.dart';
 import 'package:testflow/domain/state/requirements/requirements_state.dart';
 import 'package:testflow/presentation/common/dropdown/dropdown_input.dart';
 import 'package:testflow/presentation/common/input/text_input_field.dart';
 import 'package:testflow/presentation/common/text/title_4.dart';
+import 'package:testflow/utils/palette.dart';
 
 class RequirementsView extends StatelessWidget {
   final RequirementsState state;
@@ -19,7 +21,11 @@ class RequirementsView extends StatelessWidget {
     return StateProvider<RequirementsState>(
       state: state,
       builder: (state, context) => Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.only(
+          top: 16,
+          left: 16,
+          right: 16,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -84,6 +90,205 @@ class Table extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SizedBox();
+    const invoices = [
+      [
+        'INV001',
+        'Paid',
+        'Credit Card',
+        r'$250.00',
+      ],
+      [
+        'INV002',
+        'Pending',
+        'PayPal',
+        r'$150.00',
+      ],
+      [
+        'INV003',
+        'Unpaid',
+        'Bank Transfer',
+        r'$350.00',
+      ],
+      [
+        'INV004',
+        'Paid',
+        'Credit Card',
+        r'$450.00',
+      ],
+      [
+        'INV005',
+        'Paid',
+        'PayPal',
+        r'$550.00',
+      ],
+      [
+        'INV006',
+        'Pending',
+        'Bank Transfer',
+        r'$200.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+      [
+        'INV007',
+        'Unpaid',
+        'Credit Card',
+        r'$300.00',
+      ],
+    ];
+
+    final headings = [
+      'Invoice',
+      'Status',
+      'Method',
+      'Amount',
+    ];
+
+    return Expanded(
+      child: ShadTable(
+        columnCount: invoices[0].length,
+        rowCount: invoices.length,
+        header: (context, column) => ShadTableCell.header(
+          child: Text(headings[column]),
+        ),
+        columnSpanExtent: (index) => const FractionalSpanExtent(0.25),
+        onRowTap: (index) => print(invoices[index]),
+        rowSpanBackgroundDecoration: (row) => row == 0
+            ? const SpanDecoration(
+                color: Palette.backgroundTableHeader,
+              )
+            : null,
+        builder: (context, index) {
+          final List<String> invoice = invoices[index.row];
+          return ShadTableCell(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              invoice[index.column],
+              style: index.column == 0
+                  ? const TextStyle(fontWeight: FontWeight.w500)
+                  : null,
+            ),
+          );
+        },
+      ),
+    );
   }
 }
--- frontend/lib/utils/palette.dart
+++ frontend/lib/utils/palette.dart
@@ -9,6 +9,7 @@ class Palette {
 
   static const Color background1 = Color(0xffffffff);
   static const Color background2 = Color(0xffF9FAFC);
+  static const Color backgroundTableHeader = Color(0xfff0f0f0);
 
   static const Color iconEnabled = Color(0xff222222);
   static const Color iconDisabled = Color(0xff888888);

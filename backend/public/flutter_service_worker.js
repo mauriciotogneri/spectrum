086a46c7601283fc9000991927f578c49826d1d3
Mauricio Togneri <mauricio.togneri@gmail.com>
2025-02-22 18:58:06 +0100

    Improvements

--- backend/public/flutter_service_worker.js
+++ backend/public/flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "562eb1b7ac6e5dfc9ad67f72f647c8f4",
-"main.dart.js": "605e89b662d772f5e92cf0c3ef40531f",
+const RESOURCES = {"flutter_bootstrap.js": "a1d6dd7a896384c3581505b41c1607b0",
+"main.dart.js": "cc70937db150869a9bd13fd99411574c",
 "assets/NOTICES": "b475fb1e71f6902e7e45082bdc5c8adf",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/assets/fonts/Outfit_Bold.ttf": "e788bf14fc28eda0d0c134438d5a99de",
@@ -12,7 +12,7 @@ const RESOURCES = {"flutter_bootstrap.js": "562eb1b7ac6e5dfc9ad67f72f647c8f4",
 "assets/assets/fonts/Outfit_Regular.ttf": "8b7f83ac329f6b122ad42fe702a804ae",
 "assets/assets/fonts/Outfit_Light.ttf": "0a829f6fbb00ca9ddb4c0aa543ee871e",
 "assets/assets/fonts/Outfit_SemiBold.ttf": "25a374e909112c12d5328e8a4f13dda3",
-"assets/fonts/MaterialIcons-Regular.otf": "15e7c5e427c1abe83234b1434668e0e5",
+"assets/fonts/MaterialIcons-Regular.otf": "da1322108c7dee3b23dabe5b06a3f28f",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "14b5b9230084d3d28b2f5751ef678403",
 "assets/AssetManifest.json": "d8e1e586d6bf0a623da6f6a5a8297c1a",
